9377c2fe7bcf7640330cdddeb16f56fb6ba7a291
Maret Idris <maret@fasterlap.com>
2020-12-20 09:39:02 -0800

    Remove emoji on the banner section

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,9 +20,9 @@ class Home extends React.Component {
       <React.Fragment>
         <Banner
           strings={[
-            "am a product designer 🌺",
-            "express ideas through code 💻",
-            "am startup tested 💪",
+            "am a product designer",
+            "express ideas through code",
+            "am startup tested",
           ]}
         />
         <DesignProjects
